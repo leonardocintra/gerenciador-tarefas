ed5eaafbd9e5023502f7d6e02e165d277709b750
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-07-08 15:30:58 -0300

    templates home ok

--- src/main/java/com/leaolabs/tarefas/controller/HomeController.java
+++ src/main/java/com/leaolabs/tarefas/controller/HomeController.java
@@ -1,8 +1,17 @@
 package com.leaolabs.tarefas.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
+	@GetMapping("/")
+	public ModelAndView home() {
+		ModelAndView modelView = new ModelAndView();
+		modelView.setViewName("home/home");
+		modelView.addObject("mensagem", "Mensagem do Controller");
+		return modelView;
+	}
 }
