a883e3f021f001e9270afa42ff2c62169253bd9a
Leonardo Cintra <leonardo.ncintra@outlook.com>
2019-07-12 13:42:23 -0300

    post ok

--- src/main/java/com/leaolabs/tarefas/controller/TarefaController.java
+++ src/main/java/com/leaolabs/tarefas/controller/TarefaController.java
@@ -1,11 +1,18 @@
 package com.leaolabs.tarefas.controller;
 
+import java.util.Date;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.leaolabs.tarefas.model.Tarefa;
 import com.leaolabs.tarefas.repositorios.TarefaRepository;
 
 @Controller
@@ -22,5 +29,34 @@ public class TarefaController {
 		mv.addObject("tarefas", tarefaRepositorio.findAll());
 		return mv;
 	}
+
+	@GetMapping("/inserir")
+	public ModelAndView inserir() {
+		var mv = new ModelAndView();
+		mv.setViewName("tarefas/inserir");
+		mv.addObject("tarefa", new Tarefa());
+		return mv;
+	}
+
+	@PostMapping("/inserir")
+	public ModelAndView inserir(@Valid Tarefa tarefa, BindingResult result) {
+		var mv = new ModelAndView();
+
+		if (tarefa.getDataExpiracao() == null) {
+			result.rejectValue("dataExpiracao", "tarefas.dataNull", "A data de expiração não pode ser null");
+		} else if (tarefa.getDataExpiracao().before(new Date())) {
+			result.rejectValue("dataExpiracao", "tarefas.dataExpiracaoInvalida",
+					"A data de expiração não pode ser menor que a data atual");
+		} else if (result.hasErrors()) {
+			mv.setViewName("/tarefas/inserir");
+			mv.addObject(tarefa);
+		} else {
+			mv.setViewName("redirect:/tarefas/listar");
+			tarefaRepositorio.save(tarefa);
+		}
+		return mv;
+	}
+	
+	// parou no 1:25:21 https://lp.treinaweb.com.br/semana/aula-2
 
 }
--- src/main/java/com/leaolabs/tarefas/model/Tarefa.java
+++ src/main/java/com/leaolabs/tarefas/model/Tarefa.java
@@ -8,24 +8,31 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "tarefas")
 public class Tarefa {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(length = 50, nullable = false)
+	@NotNull(message = "O titulo é obrigatorio")
+	@Length(max = 50, min = 3, message = "O titulo deve ter de 3 a 50 caracteres")
 	private String titulo;
 	
-
 	@Column(length = 100, nullable = false)
+	@Length(max = 100, message = "A descrição deve no maximo 100 caracteres")
 	private String descricao;
 	
 
 	@Column(name = "data_expiracao", nullable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date dataExpiracao;
 	
 	@Column(nullable = false)
